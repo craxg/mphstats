031fed72fc2f11a6589095a64950e144aaf38d37
Xander <clientenraad+xander@rondomzorg.nl>
2017-12-19 06:45:34 +0100

    Added backtracking and fixed musicoin bug.

--- balance.py
+++ balance.py
@@ -6,6 +6,8 @@ import json
 import time
 from datetime import datetime, timedelta
 from operator import mul
+import sys, traceback
+
 
 # print "\033[H\033[J"
 
@@ -21,7 +23,7 @@ coins = {
     'ethereum': 'ETH',
     'ethereum-classic': 'ETC',
     'expanse': 'EXP',
-    'musiccoin': 'MUSIC',
+    'musicoin': 'MUSIC',
 
     'groestlcoin': 'GRS',
 
@@ -166,9 +168,26 @@ def main():
 
         output()
     except Exception, e:
-        vars(e)
-        print(e)
-        pass
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        print "*** print_tb:"
+        traceback.print_tb(exc_traceback, limit=1, file=sys.stdout)
+        print "*** print_exception:"
+        traceback.print_exception(exc_type, exc_value, exc_traceback,
+                                  limit=2, file=sys.stdout)
+        print "*** print_exc:"
+        traceback.print_exc()
+        print "*** format_exc, first and last line:"
+        formatted_lines = traceback.format_exc().splitlines()
+        print formatted_lines[0]
+        print formatted_lines[-1]
+        print "*** format_exception:"
+        print repr(traceback.format_exception(exc_type, exc_value,
+                                              exc_traceback))
+        print "*** extract_tb:"
+        print repr(traceback.extract_tb(exc_traceback))
+        print "*** format_tb:"
+        print repr(traceback.format_tb(exc_traceback))
+        print "*** tb_lineno:", exc_traceback.tb_lineno
 
 
 if __name__ == "__main__":
